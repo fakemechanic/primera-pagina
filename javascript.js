d5a894a1b1ab8998dd219408d89dec1b16fbcb8c
fakemechanic <ferhbrito@gmail.com>
2019-11-12 00:04:03 -0300

    Primer commit

---
+++ javascript.js
@@ -0,0 +1,34 @@
+const boton = document.getElementById("siguiente-pagina");
+
+boton.onclick = saludar;
+
+function saludar() {
+    // alert("Hola usuario");
+    // boton.classList.remove("boton");
+    // boton.classList.add("verde");
+    const div = document.getElementById("texto");
+
+    array.forEach(function(elemento) {
+        const p = document.createElement("p");
+        p.textContent = elemento;
+        div.appendChild(p);
+    });
+}
+
+
+const array = ["Martin", 1, "Quintana", 500];
+
+
+// Manejar el campo de texto
+const nombre = document.getElementById("nombre");
+const enviar = document.getElementById("enviar");
+
+enviar.onclick = mostrarNombre;
+
+function mostrarNombre() {
+    if (!nombre.value) {
+        return alert("Por favor ingrese el nombre");
+    }
+
+    alert(`Bienvenido ${nombre.value}`);
+}
