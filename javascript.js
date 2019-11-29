00e716da6434c9e3464a3965e14ee6abe9b8f3f9
Martin Quintana <quintanamartine@gmail.com>
2019-11-29 00:45:10 -0300

    Agrego prettier y eslint al projecto para tener linting y formateo automatico del codigo.

--- javascript.js
+++ javascript.js
@@ -1,34 +1,32 @@
-const boton = document.getElementById("siguiente-pagina");
+const boton = document.getElementById('siguiente-pagina')
 
-boton.onclick = saludar;
+boton.onclick = saludar
 
 function saludar() {
-    // alert("Hola usuario");
-    // boton.classList.remove("boton");
-    // boton.classList.add("verde");
-    const div = document.getElementById("texto");
+  // alert("Hola usuario");
+  // boton.classList.remove("boton");
+  // boton.classList.add("verde");
+  const div = document.getElementById('texto')
 
-    array.forEach(function(elemento) {
-        const p = document.createElement("p");
-        p.textContent = elemento;
-        div.appendChild(p);
-    });
+  array.forEach(function(elemento) {
+    const p = document.createElement('p')
+    p.textContent = elemento
+    div.appendChild(p)
+  })
 }
 
-
-const array = ["Martin", 1, "Quintana", 500];
-
+const array = ['Martin', 1, 'Quintana', 500]
 
 // Manejar el campo de texto
-const nombre = document.getElementById("nombre");
-const enviar = document.getElementById("enviar");
+const nombre = document.getElementById('nombre')
+const enviar = document.getElementById('enviar')
 
-enviar.onclick = mostrarNombre;
+enviar.onclick = mostrarNombre
 
 function mostrarNombre() {
-    if (!nombre.value) {
-        return alert("Por favor ingrese el nombre");
-    }
+  if (!nombre.value) {
+    return alert('Por favor ingrese el nombre')
+  }
 
-    alert(`Bienvenido ${nombre.value}`);
+  alert(`Bienvenido ${nombre.value}`)
 }
